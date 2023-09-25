6c5f5a2189cd4108db5abf8d4db99563b236be2b
destu3 <obose03@gmail.com>
2023-09-25 19:07:22 +0100

    implemented all album methods

--- package/classes/resources/Album.js
+++ package/classes/resources/Album.js
@@ -16,13 +16,46 @@ class Album {
   static baseUrl = 'https://api.spotify.com/v1/albums';
 
   /**
-   * Gets album details by its ID.
+   * Get Spotify catalog information for a single album.
    * @param {string} id - The ID of the album to retrieve.
    * @param {string} accessToken - The access token for authentication.
-   * @returns {Promise<any>} A Promise that resolves to the album data or rejects with an error.
+   * @param {object} queryStringParams - Optional query string parameters for this endpoint.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @returns {Promise<any>} A Promise that resolves with album data or error information
    */
-  static getAlbum(id, accessToken) {
-    const url = `${Album.baseUrl}/${id}`;
+  static getAlbum(id, accessToken, queryStringParams) {
+    const queryParams = new URLSearchParams(queryStringParams).toString();
+    const url = `${Album.baseUrl}/${id}?${queryParams}`;
+    return NetworkUtils.makeRequest(url, accessToken);
+  }
+
+  /**
+   * Get Spotify catalog information for multiple albums identified by their Spotify IDs.
+   * @param {string} accessToken - The access token for authentication.
+   * @param {object} queryStringParams - Query string parameters for this endpoint.
+   * @param {string} queryStringParams.ids - A comma-separated list of the Spotify IDs for the albums. Maximum: 20 IDs.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @returns {Promise<any>} A Promise that resolves with a collection of album data or error information
+   */
+  static getSeveralAlbums(accessToken, queryStringParams) {
+    const queryParams = new URLSearchParams(queryStringParams).toString();
+    const url = `${Album.baseUrl}?${queryParams}`;
+    return NetworkUtils.makeRequest(url, accessToken);
+  }
+
+  /**
+   * Get Spotify catalog information about an album’s tracks. Optional parameters can be used to limit the number of tracks returned.
+   * @param {string} id - The ID of the album to retrieve.
+   * @param {string} accessToken - The access token for authentication.
+   * @param {object} queryStringParams - Optional query string parameters for this endpoint.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @param {number} queryStringParams.limit - The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+   * @param {number} queryStringParams.offset - The index of the first item to return. Default: 0 (the first item). Use with limit to get the next set of items.
+   * @returns {Promise<any>} A Promise that resolves with album tracks or error information
+   */
+  static getAlbumTracks(id, accessToken, queryStringParams) {
+    const queryParams = new URLSearchParams(queryStringParams).toString();
+    const url = `${Album.baseUrl}/${id}/tracks?${queryParams}`;
     return NetworkUtils.makeRequest(url, accessToken);
   }
 }
--- package/index.js
+++ package/index.js
@@ -16,13 +16,48 @@ class SpotifyWrapper {
   }
 
   /**
-   * Gets album details by its ID.
+   * Get Spotify catalog information for a single album.
    * @param {string} id - The ID of the album to retrieve.
-   * @returns {Promise<any>} A Promise that resolves to the album data or rejects with an error.
+   * @param {object} queryStringParams - Optional query string parameters for this endpoint.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @returns {Promise<any>} A Promise that resolves with album data or error information
    */
-  async getAlbum(id) {
+  async getAlbum(id, queryStringParams) {
     await this.authHandler.verifyToken();
-    return Album.getAlbum(id, this.authHandler.accessToken);
+    return Album.getAlbum(id, this.authHandler.accessToken, queryStringParams);
+  }
+
+  /**
+   * Get Spotify catalog information for multiple albums identified by their Spotify IDs.
+   * @param {object} queryStringParams - Query string parameters for this endpoint.
+   * @param {string} queryStringParams.ids - A comma-separated list of the Spotify IDs for the albums. Maximum: 20 IDs.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @returns {Promise<any>} A Promise that resolves with a collection of album data or error information
+   */
+  async getSeveralAlbums(queryStringParams) {
+    await this.authHandler.verifyToken();
+    return Album.getSeveralAlbums(
+      this.authHandler.accessToken,
+      queryStringParams
+    );
+  }
+
+  /**
+   * Get Spotify catalog information about an album’s tracks. Optional parameters can be used to limit the number of tracks returned.
+   * @param {string} id - The ID of the album to retrieve.
+   * @param {object} queryStringParams - Optional query string parameters for this endpoint.
+   * @param {string} queryStringParams.market - An ISO 3166-1 alpha-2 country code.
+   * @param {number} queryStringParams.limit - The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+   * @param {number} queryStringParams.offset - The index of the first item to return. Default: 0 (the first item). Use with limit to get the next set of items.
+   * @returns {Promise<any>} A Promise that resolves with album tracks or error information
+   */
+  async getAlbumTracks(id, queryStringParams) {
+    await this.authHandler.verifyToken();
+    return Album.getAlbumTracks(
+      id,
+      this.authHandler.accessToken,
+      queryStringParams
+    );
   }
 }
 
